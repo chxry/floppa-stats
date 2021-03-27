2d5bc20067edaece37da11ac38d548a33a25292c
chxry <thechxry@gmail.com>
2021-03-27 23:13:11 +0000

    fixed!

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ import PlayerCard from "./PlayerCard";
 const App = () => {
   const players = [
     "890d1d92-55de-4a88-b1b8-002ac0be4b12",
-      "5ca3924d-f092-4f75-8e75-77432cb61728",
+    "5ca3924d-f092-4f75-8e75-77432cb61728",
   ];
 
   return (
